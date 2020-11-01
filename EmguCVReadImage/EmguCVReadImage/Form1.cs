2169b257ef9e4af24bf15d143cfe79bdb10566e9
marslanali <marslanali800@gmail.com>
2020-11-02 02:00:51 +0500

    EmguCV ReadImage added

---
+++ EmguCVReadImage/EmguCVReadImage/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace EmguCVReadImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    Image<Bgr, byte> imgInput = new Image<Bgr, byte>(ofd.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+        
+            }
+        }
+    }
+}
